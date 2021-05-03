991ab440991690be46cdfe9dd2eb38e74aee597b
Murali <murali583@sasi.ac.in>
2021-05-03 09:27:10 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,44 +1,58 @@
-//import logo from './logo.svg';
- import './App.css';
-import React, { useState } from 'react'
-import { TiDelete } from "react-icons/ti";
-//import './styles.css'
-function App() {
-  const [input, setinput] = useState("")
-  const [todos, settodos] = useState([])
-
-  const Addtodo = (e) => {
-    settodos([...todos, input])
-    setinput("")
-    e.preventDefault()
-    const key = Date.now
-    console.log(key)
-  }
- const DeleteTodo=(index)=>{
-
-    const Tododata=todos.filter((todo, i) => index !== i);
-   
-    settodos(Tododata)
-}
+// //import logo from './logo.svg';
+import React from 'react'
+import Todolist from './Components/Todolist'
+const App = () => {
   return (
-    <div className="App">
-      <form id="to-do-form">
-        <input placeholder="enter todo" type="text" value={input} onChange={(e) => setinput(e.target.value)} />
-        <button type="submit" onClick={Addtodo}>add</button>
-      </form>
-      {todos.map((todo,index) => (
-        <ul className="list" key={todo} >
-          {todo}
-          <span >
-            <TiDelete className="TiDelete" key={index} onClick={() => DeleteTodo(index)} />
-          </span>
-          {/* <span >
-            <TiDelete className="faicons" key={index} onClick={() => CreateTodo(index)} />
-          </span> */}
-        </ul>
-      ))}
+    <div>
+      <Todolist/>
     </div>
   )
 }
 
 export default App
+
+// import Todolist from './Components/Todolist'
+//  import './App.css';
+// import React, { useState } from 'react'
+// import { TiDelete } from "react-icons/ti";
+// //import './styles.css'
+// function App() {
+//   const [input, setinput] = useState("")
+//   const [todos, settodos] = useState([])
+
+//   const Addtodo = (e) => {
+//     settodos([...todos, input])
+//     setinput("")
+//     e.preventDefault()
+//     const key = Date.now
+//     console.log(key)
+//   }
+//  const DeleteTodo=(index)=>{
+
+//     const Tododata=todos.filter((todo, i) => index !== i);
+   
+//     settodos(Tododata)
+// }
+//   return (
+//     <div className="App">
+//       <form id="to-do-form">
+//         <input placeholder="enter todo" type="text" value={input} onChange={(e) => setinput(e.target.value)} />
+//         <button type="submit" onClick={Addtodo}>add</button>
+//       </form>
+//       {todos.map((todo,index) => (
+//         <ul className="list" key={todo} >
+//           {todo}
+//           <span >
+//             <TiDelete className="TiDelete" key={index} onClick={() => DeleteTodo(index)} />
+//           </span>
+//           {/* <span >
+//             <TiDelete className="faicons" key={index} onClick={() => CreateTodo(index)} />
+//           </span> */}
+//         </ul>
+//       ))}
+//       <Todolist/>
+//     </div>
+//   )
+// }
+
+// export default App
--- src/Components/Todolist/Container.js
+++ src/Components/Todolist/Container.js
@@ -1,12 +1,39 @@
-import React from 'react'
-import Presentation from './Presentation'
+import React, { useState } from 'react'
+import Presentation from "./Presentation";
 const Container = () => {
-    
-    return (
-        <div>
-            <Presentation/>
-        </div>
-    )
+  const [input, setinput] = useState("");
+  const [todos, settodos] = useState([]);
+const handleInput=(e)=>{
+   setinput(e.target.value)
 }
+const handleTodos=(e)=>{
+    settodos(e.target.value)
+}
+  const Addtodo = (e) => {
+    settodos([...todos, input]);
+    setinput("");
+    e.preventDefault();
+    const key = Date.now;
+    console.log(key);
+  };
+  const DeleteTodo = (index) => {
+    const Tododata = todos.filter((todo, i) => index !== i);
 
-export default Container
+    settodos(Tododata);
+  };
+  return (
+    <div>
+      <Presentation 
+      input={input}
+      todos={todos}
+      Addtodo={Addtodo}
+      handleInput={handleInput}
+      handleTodos={handleTodos}
+      settodos={settodos}
+      DeleteTodo={DeleteTodo}
+      />
+    </div>
+  );
+};
+
+export default Container;
